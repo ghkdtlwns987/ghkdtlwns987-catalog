d46aeb363f5dcc2f02b84b0c847d7ad291c86386
ghkdtlwns987 <ghkdtlwns987@naver.com>
2024-02-01 23:20:16 +0900

    Command, Query 분리

--- src/main/java/com/ghkdtlwns987/catalog/CatalogApplication.java
+++ src/main/java/com/ghkdtlwns987/catalog/CatalogApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class CatalogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CatalogApplication.class, args);
     }
